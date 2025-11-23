42b1eb8d16ed5fe5d2b2644282c2cbf2138c302c
Carl Jayson Rico <carljaysonrico@gmail.com>
2025-11-23 17:33:33 +0800

    fixed bg

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import PlayfulBackground from "../components/PlayfulBackground";
+import PlayfulBackground from "../components/temp";
 
 export default function Landing() {
     return (
